fbe4ec79c417ab5f26712ba127d23bd57240c22e
JakeMachulcz <158092846+JakeMachulcz@users.noreply.github.com>
2024-03-05 11:08:50 -0500

    Added a 'session' instantiation
    Added a session so that when we log in we're operating under that user.

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var session = require('express-session');
+var mysql = require('mysql');
 
 var app = express();
-
+var port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/math', (req, res) => {
-  res.render('math');
-});
-app.get('/math-results', (req, res) => {
-  res.render('math-results');
-});
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: false,
+}));
 
 app.use('/', usersRouter);
 
