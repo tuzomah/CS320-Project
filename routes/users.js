2c3a2f70bd2eb0bf3a5016c2e92864e7e7766b3a
JakeMachulcz <158092846+JakeMachulcz@users.noreply.github.com>
2024-03-05 11:11:24 -0500

    Added the login feature
    FINALLY figured out the login page.  When the site is accessed, it will send you to the login page.  If it's a successful login, it will send you to the home page. If it's not, you we be sent back to the login page.

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const session = require('express-session');
 
 router.get('/', (req, res) => {
+  const sessionData = req.session;
+  res.redirect('/login');
+});
+
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.post('/checklogin', (req, res, next) => {
+router.post('/login', (req, res) => {
   const username = req.body.usrnm;
   const password = req.body.psswrd;
   const sql = 'SELECT * FROM Logins WHERE Username = ? AND Password = ?';
 
-  db.query(sql, [`${username}`], [`${password}`], (err, results) => {
-    if (err) throw err;
-    else if (results.length > 0) {
-      //res.redirect('/home');
-      window.location.href = 'resume.ejs'
+  db.query(sql, [`${username}`, `${password}`], (err, results) => {
+    if (results.length > 0) {
+      res.redirect('/home');
     }
     else {
-      res.send('Invalid login.');
+      res.redirect('/login');
+    }
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/login');
     }
   });
 });
 
 router.get('/home', (req, res) => {
-  res.render('resume');
+    res.render('resume');
 });
 
 router.get('/search', (req, res) => {
@@ -45,19 +59,6 @@ router.post('/search-results', (req, res) => {
     if (err) throw err;
     res.render('search-results', { title: 'Search Results', userData: data });
   });
-});
-
-router.post('/math-results', (req, res) => {
-  var num1 = parseFloat(req.body.num1);
-  var num2 = parseFloat(req.body.num2);
-  var num3 = parseFloat(req.body.num3);
-  var mult = num1 * num2 * num3;
-  var sum = num1 + num2 + num3;
-
-  var multResults = num1 + ' * ' + num2 + ' * ' + num3 + ' = ' + mult;
-  var sumResults = num1 + ' + ' + num2 + ' + ' + num3 + ' = ' + sum;
-
-  res.render('math-results', {  multResults, sumResults })
 });
 
 module.exports = router;
